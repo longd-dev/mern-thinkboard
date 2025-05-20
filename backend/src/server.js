e9a4210c8683ad83b0bef8f511c59ec0f16bace5
longd-dev <longduong325@gmail.com>
2025-05-20 11:53:38 -0700

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+//middleware
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.json()); // this middleware with parse json bodies: req.body
+app.use(rateLimiter);
+
+//simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`req method is ${req.method} & req url is ${req.url}`);
+//   next();
+// });
+
+app.use("/api/notes", notesRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
+});
